1cdf1b2787082ecb70c9094599395a72cdfe8b4e
dm1tro69 <dimolg22@gmail.com>
2021-07-25 23:51:11 +0300

    add list evends

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,29 @@
+
 import Layout from "../../components/Layout";
+import EventItem from "../../components/EventItem";
+import {API_URL} from "../../config";
 
 
-const EventsPage = () => {
+export default function EventsPage({events}) {
+    console.log(events)
     return (
         <Layout>
-           <h1>My Events</h1>
-        </Layout>
-    );
-};
 
-export default EventsPage;
+            <h1>Upcoming Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+            {events.map(evt => (
+                <EventItem evt={evt} key={evt.id}>
+
+                </EventItem>
+            ))}
+        </Layout>
+    )
+}
+export async function getServerSideProps(){
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+    return {
+        props: {events},
+        // revalidate: 1,
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
-import Link from 'next/link'
 import Layout from "../components/Layout";
+import {API_URL} from "../config";
+import EventItem from "../components/EventItem";
+import Link from 'next/link'
 
 
-export default function HomePage() {
+export default function HomePage({events}) {
+    console.log(events)
   return (
     <Layout>
 
-      <h1>Home</h1>
-        <Link href={'/about'}><a>About</a></Link>
+      <h1>Upcoming Events</h1>
+        {events.length === 0 && <h3>No events to show</h3>}
+        {events.map(evt => (
+            <EventItem evt={evt} key={evt.id}>
+
+            </EventItem>
+        ))}
+        {events.length > 0 && (
+            <Link href={'/events'}>
+                <a className={'btn-secondary'}>View All Events</a>
+            </Link>
+        )}
     </Layout>
   )
+}
+export async function getServerSideProps(){
+   const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+    return {
+       props: {events: events.slice(0, 3)},
+        // revalidate: 1,
+    }
 }
